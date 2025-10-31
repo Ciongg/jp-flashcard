2638f415f64283c8b8ccb484054c8049e5b1d3a6
Ciongg <karlinciong04@gmail.com>
2025-10-31 15:15:03 +0800

    remove ziggy

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
 import './bootstrap';
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
-import {ZiggyVue} from '../../vendor/tightenco/ziggy';
 
 createInertiaApp({
   resolve: name => {
@@ -11,7 +10,6 @@ createInertiaApp({
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
-      .use(ZiggyVue)
       .mount(el)
   },
 })
